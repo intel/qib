intel/qib

commit 7b123f8b4cb0e7de5d8ffcf1350364cce04521c8

IB/qib: MADs should return failure with misset mkeys
If a MAD is sent directly to the local HCA rather than placed on a QP and the MAD fails mkey checks,
there is no means to generate a timeout for the client, which may hang.  Instead we report
IB_MAD_RESULT_FAILURE, which operates the same for on-the-wire packets, but will generate a send
failure back to the client.

Reviewed-by: Mike Marciniszyn <mike.marciniszyn@intel.com>
Signed-off-by: Jim Foraker <foraker1@llnl.gov>

META
@@ -1,5 +1,5 @@
 Meta:         1
 Name:         qib
 Version:      1.5.3
-Release:      10
+Release:      11
 Release-Tags: relext

qib_mad.c
@@ -434,7 +434,7 @@ static int check_mkey(struct qib_ibport *ibp, struct ib_smp *smp, int mad_flags)
 					ibp->mkey_lease_period * HZ;
 			/* Generate a trap notice. */
 			qib_bad_mkey(ibp, smp);
-			ret = IB_MAD_RESULT_SUCCESS | IB_MAD_RESULT_CONSUMED;
+			ret = 1;
 		}
 	}
 
@@ -465,6 +465,7 @@ static int subn_get_portinfo(struct ib_smp *smp, struct ib_device *ibdev,
 			ibp = to_iport(ibdev, port_num);
 			ret = check_mkey(ibp, smp, 0);
 			if (ret)
+				ret = IB_MAD_RESULT_FAILURE;
 				goto bail;
 		}
 	}
@@ -1904,6 +1905,7 @@ static int process_subn(struct ib_device *ibdev, int mad_flags,
 		    port_num && port_num <= ibdev->phys_port_cnt &&
 		    port != port_num)
 			(void) check_mkey(to_iport(ibdev, port_num), smp, 0);
+		ret = IB_MAD_RESULT_FAILURE;
 		goto bail;
 	}
 

commit 32a325c849a1e39e740ffc9206e4ff1b2af35187

IB/qib: fix compliance test failure C14-024#06_LocalPortNum
Commit 7b123f8b ("IB/qib: MADs with misset M_Keys should return failure")
introduced a return code assignment that unfortunately introduced
an unconditional exit for the routine due to the lack of braces.

This patch adds the braces to correct the original patch.

Reviewed-by: Dean Luick <dean.luick@intel.com>
Signed-off-by: Mike Marciniszyn <mike.marciniszyn@intel.com>

META
@@ -1,5 +1,5 @@
 Meta:         1
 Name:         qib
 Version:      1.5.3
-Release:      11
+Release:      12
 Release-Tags: relext

qib_mad.c
@@ -464,9 +464,10 @@ static int subn_get_portinfo(struct ib_smp *smp, struct ib_device *ibdev,
 		if (port_num != port) {
 			ibp = to_iport(ibdev, port_num);
 			ret = check_mkey(ibp, smp, 0);
-			if (ret)
+			if (ret) {
 				ret = IB_MAD_RESULT_FAILURE;
 				goto bail;
+			}
 		}
 	}
 